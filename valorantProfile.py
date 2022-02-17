012807744e0196ff7af37f3fd839751c5cae7857
Jan Rylan Pueblo <97818409+jrpueblo@users.noreply.github.com>
2022-02-17 04:53:39 -0500

    Added functionality for additional players
    The valorant in-game stats function now works properly for both Immortal+ players and Users with unique names (more than 1 word)

--- valorantProfile.py
+++ valorantProfile.py
@@ -2,7 +2,7 @@
 # Description: Class containing attributes and methods to fetch valorant
 # stats and information given account name and tag
 # Contributers: Jan Rylan Pueblo
-# Version 1.0
+# Version 1.1, Added functionality for Immortal+
 # January 25th, 2022
 
 from bs4 import BeautifulSoup
@@ -12,13 +12,23 @@ import string
 
 class valorantProfile:
 
+    immortalPlus = False
+
     def __init__(self, username, tag):
-        self.username = username.strip()
-        self.tag = tag
-        link = "https://tracker.gg/valorant/profile/riot/" + self.username + "%23" + self.tag + "/overview"
+        self.username = username
+        self.tag = tag[1:]
+        player = ""
+        for i in range(len(username)):
+            player += username[i] + "%20"
+        player = player[:-3]
+        link = "https://tracker.gg/valorant/profile/riot/" + player + "%23" + self.tag + "/overview"
         self.url = link.replace(" ", "")
         result = requests.get(self.url)
         self.doc = BeautifulSoup(result.text, "html.parser")
+        rank = self.getRank().split(' ')[0]
+        ranks = "ironbronzesilvergoldplatniumdiamond"
+        if(rank.lower() not in ranks):
+            self.immortalPlus = True
 
     # Accessor Methods
 
@@ -35,9 +45,18 @@ class valorantProfile:
         return self.doc
 
     def getRank(self):
+        #value -> Diamond 2, label -> Rating
+        #For Immortal+ value -> RR and label -> Immortal/Radiant
+
         stats = self.doc.find("div", class_="valorant-highlighted-stat")
         rank = stats.find("span", class_="valorant-highlighted-stat__value").text
-        return rank
+
+        if(self.immortalPlus):
+            rank = stats.find("span", class_="valorant-highlighted-stat__label").text
+            Rating = ": " + stats.find("span", class_="valorant-highlighted-stat__value").text
+            return rank + Rating
+        else:
+            return rank
 
     def getKD(self):
         stats = self.doc.find_all("div", class_="wrapper")[1]
@@ -50,7 +69,21 @@ class valorantProfile:
         return winPercentage
 
     def getMainStats(self):
-        return [self.getRank(), self.getKD(), self.getWinPercentage()]
+        if(self.immortalPlus):
+            return [self.getRank(), self.getLeaderboard(), self.getKD(), self.getWinPercentage()]
+        else:
+            return [self.getRank(), self.getKD(), self.getWinPercentage()]
+
+    # This method checks leaderboard position of Immortal+ player
+    def getLeaderboard(self):
+        stats = self.doc.find("div", class_="valorant-highlighted-content")
+        rank = stats.findAllNext("span", class_="valorant-highlighted-stat__value")[1].text
+
+        if(self.immortalPlus):
+            return rank
+        else:
+            return "This user is not Immortal+! Git Gud"
+
 
 
     # Print Methods
@@ -76,6 +109,9 @@ def main():
     user = creds.split('#')[0]
     tag = creds.split('#')[1]
     profile = valorantProfile(user, tag)
+
+
+
 
 if __name__ == "__main__":
     main()
